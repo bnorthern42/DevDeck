27edda8c61fd53d19f665f04cc65af2eeb06b4d4
Brad Northern <bradn4@gmail.com>
2025-06-21 18:17:39 -0500

    can read json data successfully

--- utils/templates.hpp
+++ utils/templates.hpp
@@ -3,12 +3,14 @@
 
 #include <QString>
 #include <QJsonDocument>
-
-
+#include <QFile>
+#include <qjsondocument.h>
+#include <qlogging.h>
+#include <QJsonObject>
+#include <QIODevice>
 //load in json templates
-class LangTemplate:
-{   private;
-       
+class LangTemplate
+{   
     public:
 
     QString name;
@@ -19,12 +21,68 @@ class LangTemplate:
     QString run_command;
     QString comment_syntax;
     int exec_timeout_secs;
+LangTemplate() = default;
+LangTemplate(const QString& LangCodeName)
+{
+    //load template into memory/
+    QString filename = LangCodeName + ".json";
+    QFile jsonFile( filename);
+    jsonFile.open(QIODevice::ReadOnly | QIODevice::Text);
+    auto jsonDoc =QJsonDocument().fromJson(jsonFile.readAll());
+    QJsonObject jsonObj = jsonDoc.object();
+    
+    this->name              = jsonObj["name"].toString();
+    this->icon              = jsonObj["icon"].toString();
+    this->file_extension    = jsonObj["file_extension"].toString();
+    this->default_code      = jsonObj["default_code"].toString();
+    this->build_command     = jsonObj["build_command"].toString();
+    this->run_command       = jsonObj["run_command"].toString();
+    this->comment_syntax    = jsonObj["comment_syntax"].toString();
+    this->exec_timeout_secs = jsonObj["exec_timeout_secs"].toInt();
 
 
-} 
-LangTemplate();
+}
 
-~LangTemplate();
+~LangTemplate()
+{
 
+}
+ QString getName()
+{
+    return name;
+}
+ QString getIcon()
+{
+    return icon;
+}
+ QString getFileExt()
+{
+    return file_extension;
+}
+ QString getDefaultCode()
+{
+    return default_code;
+}
+ QString getBuildCMD()
+{
+    return build_command;
+}
+ QString getRunCMD()
+{
+    return run_command;
+}
+ QString getCommentStax()
+{
+    return comment_syntax;
+}
+ int getTimeout()
+{
+    return exec_timeout_secs;
+}
+void PrintAll()
+{
+    qDebug() << "JSON DATA: " << name ; 
+}
+}; 
 
 #endif TEMPLATES_HPP
